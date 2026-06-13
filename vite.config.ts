000c529b36d3ba6f65bf4d4423f1e659fbfb6731
Vince Lasam <vincelasam2@gmail.com>
2026-06-13 13:23:05 +0800

    debug/fixed structure for programming runnning

--- vite.config.ts
+++ vite.config.ts
@@ -10,27 +10,22 @@ function figmaAssetResolver() {
     resolveId(id) {
       if (id.startsWith('figma:asset/')) {
         const filename = id.replace('figma:asset/', '')
-        return path.resolve(__dirname, 'src/assets', filename)
+        return path.resolve(__dirname, 'src/GlycemicTracker.Frontend/src/assets', filename)
       }
     },
   }
 }
-
 export default defineConfig({
+  root: 'src/GlycemicTracker.Frontend', 
   plugins: [
     figmaAssetResolver(),
-    // The React and Tailwind plugins are both required for Make, even if
-    // Tailwind is not being actively used – do not remove them
     react(),
     tailwindcss(),
   ],
   resolve: {
     alias: {
-      // Alias @ to the src directory
-      '@': path.resolve(__dirname, './src'),
+      '@': path.resolve(__dirname, './src/GlycemicTracker.Frontend/src'),
     },
   },
-
-  // File types to support raw imports. Never add .css, .tsx, or .ts files to this.
   assetsInclude: ['**/*.svg', '**/*.csv'],
 })
